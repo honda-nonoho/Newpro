9c866b4f92f2b08687828f608e980f91defbf6cc
honda-nonoho <nnhhnd3012@gmail.com>
2020-04-30 01:55:41 +0900

    Initial Commit

---
+++ Newpro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Newpro
+//
+//  Created by nonoho.honda on 2020/04/30.
+//  Copyright © 2020 nonoho.honda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
